2db7623d6b9b7e112396a5adb8b4436ee3428ebf
sherlyls <sherlyls862@gmail.com>
2024-01-02 15:45:36 +0700

    model product

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 const app = express()
 import dotenv from 'dotenv'
+import { connectDatabase } from './config/dbConnect.js';
 
 dotenv.config({ path: 'backend/config/config.env'})
 
-// import all routes
+//Connecting to database
+connectDatabase();
 
+// import all routes
 import productRoutes from './routes/products.js'
 
 app.use("/api/v1", productRoutes)
