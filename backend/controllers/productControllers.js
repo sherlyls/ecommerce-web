2fb2c7075d019162a4e2dab902608704dd9f08f9
sherlyls <sherlyls862@gmail.com>
2024-01-02 17:55:12 +0700

    product controller

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -3,3 +3,10 @@ export const getProducts = async (req, res) => {
         message: "All Products"
     })
 }
+
+// Create new Product => /api/v1/admin/products
+export const newProduct = async (req, res) => {
+    res.status(200).json({
+        message: "All Products"
+    });
+}
