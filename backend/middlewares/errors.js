bb03ec21ef43736646d844ebbb143c46d047c1e1
sherlyls <sherlyls862@gmail.com>
2024-01-11 14:31:51 +0700

    error dev and prod

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -4,7 +4,16 @@ export default (err, req, res, next) => {
         message: err?.message || 'Internal Server Error'
     }
 
-    res.status(error.statusCode).json({
-        message: error.message,
-    })
+    if(process.env.NODE_ENV === 'DEVELOPMENT') {
+        res.status(error.statusCode).json({
+            message: error.message,
+            error: err,
+            stack: err.stack    
+        })
+    }
+    if(process.env.NODE_ENV == 'PRODUCTION') {
+        res.status(error.statusCode).json({
+            message: error.message
+        })
+    }
 }
