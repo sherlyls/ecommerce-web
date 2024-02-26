b9ab44bf6cfaa519ffb6c1c359df7a03ae4483f7
sherlyls <sherlyls862@gmail.com>
2024-02-26 11:41:51 +0700

    hash password

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,4 +1,5 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -30,5 +31,14 @@ const userSchema = new mongoose.Schema({
 },
  { timestamps: true}
 )
+
+// Encrypting password before saving the user
+userSchema.pre('save', async function (next) {
+    if(!this.isModified("password")) {
+        next()
+    }
+
+    this.password = await bcrypt.hash(this.password, 10)
+})
 
 export default mongoose.model("User", userSchema)
